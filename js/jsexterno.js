73e6ef81780014504ada48ac4a2f778ddcc18da5
KaiOxS <torito9926@hotmail.es>
2025-11-05 19:42:34 -0500

    Taller 6 Fundamentos de JS  On branch main

--- js/jsexterno.js
+++ js/jsexterno.js
@@ -60,3 +60,104 @@ function multiplicar(num1, num2) {
 function dividir(num1, num2) {
     return num1 / num2;
 }
+
+function fundamentosJS(){
+    /* 3 Tipos de variables */
+
+    //var 
+    var nombre = "Anthony"; // Antigua, es cosiderada obsoleta
+
+    //let
+    let apellido = "Cajamarca"; // variables cambiantes
+    let apellido2 = 15;
+    apellido2 = "Terán";
+    let arreglo = [1,2,3,4,5,6];
+    let diasSemana = ["Lunes", "Martes", 'Miércoles', '......'];
+
+    //const --> Variable final
+    const IVA =  12.8;
+    console.log("Fundamentos de JS");
+    console.log(nombre);
+    console.log(IVA);
+    console.log(arreglo);
+     
+    //Arreglos --> Lo más común es declararlos como constante (const)
+    const arreglosDiasSemana = ['Lunes', 'Martes', 'Miércoles']
+    arreglosDiasSemana.push('Jueves');
+    console.log(arreglosDiasSemana);
+    arreglosDiasSemana.unshift('Días');
+    console.log(arreglosDiasSemana);    
+    console.log(arreglosDiasSemana[0]);
+    console.log('Manejo de nulos, undefined y vacío');
+    arreglosDiasSemana.push(null);
+    arreglosDiasSemana.push('');
+    console.log(arreglosDiasSemana[5]);
+    console.log(arreglosDiasSemana[6]);
+    console.log(arreglosDiasSemana[7]);
+
+    const numerosPares = [2,4,6,8];
+    const numerosImpares = [1,3,5,7,9];
+    const numerosTotales = numerosImpares.concat(numerosPares);
+    console.log(numerosTotales);
+
+    /* Sentencias de Control */
+    let edad = 19;
+    if(edad >= 18){
+        console.log('Es mayor de edad');
+    } else {
+        console.log('Es menor de edad');
+    }
+
+    let dia = 'Lunes';
+    switch(dia){
+        case 'lunes': 
+            console.log('Es Lunes');
+            break;
+        case 'martes': 
+            console.log('Es Martes');
+            break;
+        default:
+            console.log('No es ninguno de esos días');
+    }
+
+    for(let i = 0; i<=5; i++){
+        console.log(i);
+    }
+
+    const frutas = ['Manzana', 'Sandía', 'Papaya', 'Naranja'];
+    for(let fruta of frutas){ // Si se va a iterar con un arreglo se usa of
+        console.log(fruta);
+    }
+
+    /* Manejo de Objetos */
+    const profesor = {
+        nombre: 'Anthony',
+        apellido: 'Cajamarca',
+        edad: 25,
+        ecuatoriano: true,
+        genero: 'M',
+        ciudad: 'Quito'
+    }
+
+    console.log(profesor);
+    console.log(profesor.nombre);
+    profesor.apellido = 'KaiOxS';
+    console.log(profesor);
+
+    // Comparadores 
+
+    if (profesor.ciudad === 'Quito') { 
+        console.log('Es Quiteño');
+    }
+
+    if (profesor.edad !== 25) {
+        console.log('Es diferente de 25');
+    } else {
+        console.log('Es 25');
+    }
+
+    for (let clave in profesor) { // Si se otera en un objeto se usa in
+        console.log(clave);
+        console.log(profesor[clave]);
+    }
+}
